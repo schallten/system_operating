b032daf0c3ead872b374326ac2a1f4f09fb032db
schallten <166111488+schallten@users.noreply.github.com>
2026-01-26 10:16:57 +0530

    Edited arch.cpp

--- src/src/architecture/architecture.cpp
+++ src/src/architecture/architecture.cpp
@@ -1,56 +1,80 @@
 #include "architecture.h"
 #include "../core/output_handler.h" // output handled universally in the os through this
 #include <Arduino.h>
+#include <bits/stdc++.h>
+#include <LittleFS.h>
 
 // int consumes 8 bits , long long takes 16 bits
 // i think i should use int to sustain for the constraints of esp
 // total memory in a nodemcu is typically 4mb = 4 * 1024 * 1024 bits = 4194304 bits , so basically we can create 524288 integers in the memory
 // lets me only allow 1/8th of that to be used as stack size , this is a stack based os yes
+// instrction plan
+// 1 = PUSH (put a number on the stack)
+// 2 = LOAD (get a variable's value)
+// 3 = STORE (save to a variable)
+// 4 = ADD
+// 5 = SUB
+// 6 = MUL
+// 7 = DIV
+// 8 = PRINT
+// 9 = HALT (stop)
 
-// variables
-#define STACK_SIZE   1024     // 4 kb
-#define MEMORY_SIZE  2048     // 8 kb
-#define PROGRAM_SIZE 4096     // 4 kb
+int stack[64];
+int top = 0; 
 
-int stack[STACK_SIZE];      // stack memory
-int memory[MEMORY_SIZE];    // general purpose memory
-int program[PROGRAM_SIZE];  // program memory
-int pc = 0;                 // program counter , stores current instruction
-int sp = -1;                // stack pointer , aka top of stack
+int variables[256];  // 256 slots for variables
+
+int pc = 0;
+std::vector<std::string> program(1024); // set of instructions form a program , did you know the size limit is 1024 lines , because esp is memory limited
 
 
-
-// functions to handle
-
-void push(int value){
-    if (sp>=STACK_SIZE){
-        printOutput("Stack Overflow Error");
-        return;
+void push(int value) {
+    if (top < 64) {
+        stack[top++] = value;
+    }else{
+        printOutput("Stack Overflow Error\n");
     }
-    sp++;
-    stack[sp] = value;
+
 }
 
-int pop(){
-    if (sp<0){
-        printOutput("Stack Underflow Error");
-        return;
+int pop() {
+    if (top > 0) {
+        return stack[--top];
     }
-    return stack[--sp];  
+    printOutput("Stack Underflow Error\n");
+    return 0;
 }
 
-int peek(){
-    if (sp<0){
-        printOutput("Stack Underflow Error");
-        return;
+int peek() {
+    if (top > 0) {
+        return stack[top - 1];
     }
-    return stack[sp];
+    printOutput("Stack is Empty\n");
+    return 0;
 }
 
-// for now , this is suffcient , after building VM for the langguage this can continue
+void executor(std::string line){
+    // todo
+}
 
-
-
-
-
-
+void loadProgram(const std::string& filename){
+    // file will be read line by line using liitlefs to first get full file then cpp to read it line by line
+    File file = LittleFS.open(filename.c_str(), "r");
+    if (!file) {
+        printOutput("Failed to open program file\n");
+        return;
+    }
+    program.clear();
+    pc = 0;
+    String line;
+    while (file.available()) {
+        line = file.readStringUntil('\n');
+        program.push_back(line.c_str());
+        pc++;
+        // instead of interpreting line by line we will first load the program into memory then run it as a whole
+    }
+    file.close();
+    for (pc = 0; pc < program.size(); pc++){
+        executor(program[pc]);
+    }
+}
