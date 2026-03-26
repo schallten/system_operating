77d63cac3a56acb063bc392c3bdbff504c2e1f32
schallten <166111488+schallten@users.noreply.github.com>
2026-03-26 09:56:38 +0530

    pc version

--- src/src/core/output_handler.cpp
+++ src/src/core/output_handler.cpp
@@ -1,17 +1,30 @@
 // output_handler.cpp
 // Output handler implementation - routes output to serial or display
 
+//#define PC_VERSION // comment this out to disable pc version and use serial output instead
+
 #include "output_handler.h"
 #include "display.h"
 #include <Arduino.h>
+// Global graphics mode: 0 = serial, 1 = display, 2= cout ( if in case i make a pc version)
+int graphics = 0;
 
-// Global graphics mode: 0 = serial, 1 = display
+#ifdef PC_VERSION // to turn it on define PC_VERSION in the build settings or at the top of this file
+#include <iostream>
+#endif
+
 int graphics = 0;
 
 void printOutput(const String &text) {
     if (graphics == 1) {
         displayText(text.c_str());
-    } else {
+    }
+#ifdef PC_VERSION
+    else if (graphics == 2) {
+        std::cout << text << std::endl;
+    }
+#endif
+    else {
         Serial.print(text);
     }
 }
@@ -19,7 +32,13 @@ void printOutput(const String &text) {
 void printOutput(const char* text) {
     if (graphics == 1) {
         displayText(text);
-    } else {
+    }
+#ifdef PC_VERSION
+    else if (graphics == 2) {
+        std::cout << text << std::endl;
+    }
+#endif
+    else {
         Serial.print(text);
     }
 }
@@ -27,7 +46,13 @@ void printOutput(const char* text) {
 void printlnOutput(const String &text) {
     if (graphics == 1) {
         displayText(text.c_str());
-    } else {
+    }
+#ifdef PC_VERSION
+    else if (graphics == 2) {
+        std::cout << text << std::endl;
+    }
+#endif
+    else {
         Serial.println(text);
     }
 }
@@ -35,7 +60,13 @@ void printlnOutput(const String &text) {
 void printlnOutput(const char* text) {
     if (graphics == 1) {
         displayText(text);
-    } else {
+    }
+#ifdef PC_VERSION
+    else if (graphics == 2) {
+        std::cout << text << std::endl;
+    }
+#endif
+    else {
         Serial.println(text);
     }
 }
@@ -44,7 +75,13 @@ void printCharOutput(char c) {
     if (graphics == 1) {
         String charStr(c);
         displayText(charStr.c_str());
-    } else {
+    }
+#ifdef PC_VERSION
+    else if (graphics == 2) {
+        std::cout << c << std::endl;
+    }
+#endif
+    else {
         Serial.write(c);
     }
 }
